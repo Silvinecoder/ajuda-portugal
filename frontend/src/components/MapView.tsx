b7ad51527b5b2af778ed4df2f9d77b1bd9c1457e
silvinecoder <creativeservicepackages@gmail.com>
2026-02-07 18:44:49 +0000

    removed usecallback

--- frontend/src/components/MapView.tsx
+++ frontend/src/components/MapView.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
